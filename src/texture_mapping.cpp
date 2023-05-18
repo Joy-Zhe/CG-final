6463115c80244ed7bee8007671d3e7cc7536f74e
Jin Yuhang <128334322+jam-2002@users.noreply.github.com>
2023-05-18 10:16:28 +0800

    Update texture_mapping.cpp

--- src/texture_mapping.cpp
+++ src/texture_mapping.cpp
@@ -1,33 +1,32 @@
-﻿#include <imgui.h>
+#include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
 #include "texture_mapping.h"
 
-//�ӿ�
+//接口
 const std::string modelRelPath = "obj/sphere.obj";
-const std::string newmodelRelPath = "obj/castle.obj";
-const std::string earthTextureRelPath = "texture/miscellaneous/earthmap.jpg";
-const std::string planetTextureRelPath = "texture/miscellaneous/planet_Quom1200.png";
+const std::string newmodelRelPath = "obj/mario (2).obj";
+const std::string wall2 = "texture/miscellaneous/Frame 34.jpg";
+const std::string wall1= "texture/miscellaneous/F.jpg";
 
+//bk ft exchange
 const std::vector<std::string> skyboxTextureRelPaths = {
-    "texture/skybox/Right_Tex.jpg", "texture/skybox/Left_Tex.jpg",  "texture/skybox/Up_Tex.jpg",
-    "texture/skybox/Down_Tex.jpg",  "texture/skybox/Front_Tex.jpg", "texture/skybox/Back_Tex.jpg" };
+    "texture/skybox/gloomy_rt.png", "texture/skybox/gloomy_lf.png",  "texture/skybox/gloomy_up.png",
+    "texture/skybox/gloomy_dn.png",  "texture/skybox/gloomy_bk.png", "texture/skybox/gloomy_ft.png"};
 
 TextureMapping::TextureMapping(const Options& options) : Application(options) {
     // init model
 
-    _sphere.reset(new newmodel(getAssetFullPath(newmodelRelPath)));
-    _sphere->transform.scale = glm::vec3(0.08f, 0.08f, 0.08f);
-
-
+    mario.reset(new newmodel(getAssetFullPath(newmodelRelPath)));
+    mario->transform.scale = glm::vec3(0.08f,0.08f, 0.08f);
+    mario->transform.position = glm::vec3(3.0f, -4.0f, -2.0f);
+    mario->transform.rotation= { 1.0f, 0.0f, 0.0f, 0.0f };
     // init textures
     std::shared_ptr<Texture2D> earthTexture =
-        std::make_shared<ImageTexture2D>(getAssetFullPath(earthTextureRelPath));
+        std::make_shared<ImageTexture2D>(getAssetFullPath(wall1));
     std::shared_ptr<Texture2D> planetTexture =
-        std::make_shared<ImageTexture2D>(getAssetFullPath(planetTextureRelPath));
-  
-
+        std::make_shared<ImageTexture2D>(getAssetFullPath(wall2));
 
     // init materials
     _simpleMaterial.reset(new SimpleMaterial);
@@ -45,6 +44,7 @@ TextureMapping::TextureMapping(const Options& options) : Application(options) {
     _checkerMaterial->colors[0] = glm::vec3(1.0f, 1.0f, 1.0f);
     _checkerMaterial->colors[1] = glm::vec3(0.0f, 0.0f, 0.0f);
 
+    
     // init skybox
     std::vector<std::string> skyboxTextureFullPaths;
     for (size_t i = 0; i < skyboxTextureRelPaths.size(); ++i) {
@@ -52,10 +52,21 @@ TextureMapping::TextureMapping(const Options& options) : Application(options) {
     }
     _skybox.reset(new SkyBox(skyboxTextureFullPaths));
 
-    // init camera
-    _camera.reset(new PerspectiveCamera(
-        glm::radians(50.0f), 1.0f * _windowWidth / _windowHeight, 0.1f, 10000.0f));
-    _camera->transform.position.z = 10.0f;
+    // init cameras
+    _cameras.resize(2);
+
+    const float aspect = 1.0f * _windowWidth / _windowHeight;
+    constexpr float znear = 0.1f;
+    constexpr float zfar = 10000.0f;
+
+    // perspective camera
+    _cameras[0].reset(new PerspectiveCamera(glm::radians(60.0f), aspect, 0.1f, 10000.0f));
+    _cameras[0]->transform.position = glm::vec3(0.0f, 0.0f, 15.0f);
+
+    // orthographic camera
+    _cameras[1].reset(
+        new OrthographicCamera(-4.0f * aspect, 4.0f * aspect, -4.0f, 4.0f, znear, zfar));
+    _cameras[1]->transform.position = glm::vec3(0.0f, 0.0f, 15.0f);
 
     // init light
     _light.reset(new DirectionalLight());
@@ -65,7 +76,6 @@ TextureMapping::TextureMapping(const Options& options) : Application(options) {
     // init shaders
     initSimpleShader();
     initBlendShader();
-    initCheckerShader();
 
     // init imGUI
     IMGUI_CHECKVERSION();
@@ -137,11 +147,6 @@ void TextureMapping::initBlendShader() {
         "    gl_Position = projection * view * model * vec4(aPosition, 1.0f);\n"
         "}\n";
 
-    // TODO: change the fragment shader code to achieve the following goals
-    // + blend the colors of the two textures
-    // + lambert shading, i.e the color is affected by the light
-    // write your code here
-    // -----------------------------------------------------------------
     const char* fsCode =
         "#version 330 core\n"
         "in vec3 fPosition;\n"
@@ -165,32 +170,28 @@ void TextureMapping::initBlendShader() {
         "uniform sampler2D mapKds[2];\n"
 
         "void main() {\n"
-        // "  color = vec4(material.kds[0], 1.0f);\n"
-         // �����������в�����ɫ���������ɸ�����ɫ
+       // "  color = vec4(material.kds[0], 1.0f);\n"
+        // 从两个纹理中采样颜色，且纹理可富有颜色
         "vec4 color1 = texture(mapKds[0], fTexCoord)*vec4(material.kds[0],1.0);"
         "vec4 color2 = texture(mapKds[1], fTexCoord)*vec4(material.kds[1],1.0);"
 
-        // ���������������ɫ
+         // 混合两个纹理的颜色
         "vec4 blendedColor = mix(color1, color2, material.blend);"
 
-        // ���㷴�����ǿ��
-        "    vec3 N = normalize(fNormal);"
+        // 计算反射光照强度
+        "   vec3 N = normalize(fNormal);"
         "    vec3 L = normalize(-light.direction);"
         "    float diff = max(dot(N, L), 0.0);"
         "    vec3 diffuse = diff * light.color * light.intensity * blendedColor.rgb;"
 
-        // ���㻷�����յ�Ӱ��
+        // 计算环境光照的影响
         "    vec3 ambient = light.color * light.intensity * 0.1 * blendedColor.rgb;"
 
-        // ����������ɫ
+        // 计算最终颜色
         "    vec3 finalColor = ambient + diffuse;"
         "    color = vec4(finalColor, blendedColor.a);"
-
-
+       
         "}\n";
-
-
-    //----------------------------------------------------------------
 
     _blendShader.reset(new GLSLProgram);
     _blendShader->attachVertexShader(vsCode);
@@ -199,64 +200,102 @@ void TextureMapping::initBlendShader() {
 
 }
 
-void TextureMapping::initCheckerShader() {
-    const char* vsCode =
-        "#version 330 core\n"
-        "layout(location = 0) in vec3 aPosition;\n"
-        "layout(location = 1) in vec3 aNormal;\n"
-        "layout(location = 2) in vec2 aTexCoord;\n"
-        "out vec2 fTexCoord;\n"
-        "uniform mat4 projection;\n"
-        "uniform mat4 view;\n"
-        "uniform mat4 model;\n"
-        "void main() {\n"
-        "    fTexCoord = aTexCoord;\n"
-        "    gl_Position = projection * view * model * vec4(aPosition, 1.0f);\n"
-        "}\n";
-
-    // TODO: change the following code to achieve the procedural checker texture
-    // hint: use the fTexCoord to determine the color
-    // modify your code here
-    // --------------------------------------------------------------
-    const char* fsCode =
-        "#version 330 core\n"
-        "in vec2 fTexCoord;\n"
-        "out vec4 color;\n"
-
-        "struct Material {\n"
-        "    vec3 colors[2];\n"
-        "    int repeat;\n"
-        "};\n"
-
-        "uniform Material material;\n"
-
-        "void main() {\n"
-        // ���㵱ǰ�����Ӧ�����̸�
-        "vec2 checkerCoord = floor(fTexCoord * material.repeat);\n"
-        // ʹ�� mod() �����жϵ�ǰ���̸��Ƿ�Ϊż��
-        "bool isEven = mod(checkerCoord.x+checkerCoord.y,2.0)==0;"
-        // ʹ�����������ѡ����ȷ����ɫ
-        "    color = vec4(isEven?material.colors[0]:material.colors[1], 1.0f);\n"
-        "}\n";
-
-    //----------------------------------------------------------------
-
-    _checkerShader.reset(new GLSLProgram);
-    _checkerShader->attachVertexShader(vsCode);
-    _checkerShader->attachFragmentShader(fsCode);
-    _checkerShader->link();
-}
 
 void TextureMapping::handleInput() {
+
+    // set input mode
+    /*
+    glfwSetInputMode(_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    _input.mouse.move.xNow = _input.mouse.move.xOld = 0.5f * _windowWidth;
+    _input.mouse.move.yNow = _input.mouse.move.yOld = 0.5f * _windowHeight;
+    glfwSetCursorPos(_window, _input.mouse.move.xNow, _input.mouse.move.yNow);
+    */
+    constexpr float cameraMoveSpeed = 5.0f;
+    constexpr float cameraRotateSpeed = 0.02f;
+
     if (_input.keyboard.keyStates[GLFW_KEY_ESCAPE] != GLFW_RELEASE) {
         glfwSetWindowShouldClose(_window, true);
         return;
     }
 
+    if (_input.keyboard.keyStates[GLFW_KEY_SPACE] == GLFW_PRESS) {
+        std::cout << "switch camera" << std::endl;
+        // switch camera
+        activeCameraIndex = (activeCameraIndex + 1) % _cameras.size();
+        _input.keyboard.keyStates[GLFW_KEY_SPACE] = GLFW_RELEASE;
+        return;
+    }
+
+    Camera* camera = _cameras[activeCameraIndex].get();
+
+    if (_input.keyboard.keyStates[GLFW_KEY_W] != GLFW_RELEASE) {
+        std::cout << "W" << std::endl;
+        camera->transform.position.z -= cameraMoveSpeed * 0.02;
+    }
+
+    if (_input.keyboard.keyStates[GLFW_KEY_A] != GLFW_RELEASE) {
+        std::cout << "A" << std::endl;
+        camera->transform.position.x -= cameraMoveSpeed * 0.02;
+    }
+
+    if (_input.keyboard.keyStates[GLFW_KEY_S] != GLFW_RELEASE) {
+        std::cout << "S" << std::endl;
+        camera->transform.position.z += cameraMoveSpeed * 0.02;
+    }
+
+    if (_input.keyboard.keyStates[GLFW_KEY_D] != GLFW_RELEASE) {
+        std::cout << "D" << std::endl;
+        camera->transform.position.x += cameraMoveSpeed * 0.02;
+    }
+
+    /*
+    if (_input.mouse.move.xNow != _input.mouse.move.xOld) {
+        std::cout << "mouse move in x direction" << std::endl;
+
+        float mouse_x = _input.mouse.move.xNow - _input.mouse.move.xOld;
+        glm::quat original_rotation = camera->transform.rotation;
+
+        if (mouse_x > 0)
+        {
+            camera->transform.rotation = glm::rotate(camera->transform.rotation, cameraRotateSpeed * mouse_x * 0.02f, glm::vec3(0.0f, 1.0f, 0.0f));
+        }
+        else
+        {
+            camera->transform.rotation = glm::rotate(camera->transform.rotation, cameraRotateSpeed * mouse_x * -0.02f, glm::vec3(0.0f, 1.0f, 0.0f));
+        }
+
+        glm::vec3 rotate_axis = glm::normalize(glm::cross(original_rotation * glm::vec3(1, 0, 0), camera->transform.getRight())); // Calculate the rotation axis by taking the cross product of two vectors
+
+        // Rotate the camera around the calculated rotation axis
+        camera->transform.rotation = glm::rotate(original_rotation, cameraRotateSpeed * mouse_x * 0.02f, rotate_axis);
+
+    }
+
+    if (_input.mouse.move.yNow != _input.mouse.move.yOld) {
+        std::cout << "mouse move in y direction" << std::endl;
+        float mouse_y = _input.mouse.move.yNow - _input.mouse.move.yOld;
+        glm::vec3 forward = -camera->transform.getFront();
+        glm::quat original_rotation = camera->transform.rotation;
+
+        if (mouse_y > 0)
+        {
+            camera->transform.rotation = glm::rotate(camera->transform.rotation, cameraRotateSpeed * mouse_y * 0.02f, camera->transform.getRight());
+        }
+        else
+        {
+            camera->transform.rotation = glm::rotate(camera->transform.rotation, cameraRotateSpeed * mouse_y * -0.02f, camera->transform.getRight());
+        }
+        glm::vec3 new_forward = -camera->transform.getFront();  // Get the new forward vector after rotating
+        glm::vec3 rotate_axis = glm::cross(forward, new_forward); // Calculate the rotation axis by taking the cross product of the original and new forward vectors
+        camera->transform.rotation = glm::rotate(original_rotation, cameraRotateSpeed * mouse_y * 0.02f, rotate_axis);
+
+    }
+    */
+
     const float angluarVelocity = 0.1f;
     const float angle = angluarVelocity * static_cast<float>(_deltaTime);
     const glm::vec3 axis = glm::vec3(0.0f, 1.0f, 0.0f);
-    _sphere->transform.rotation = glm::angleAxis(angle, axis) * _sphere->transform.rotation;
+    mario->transform.rotation = glm::angleAxis(angle, axis) * mario->transform.rotation;
 }
 
 void TextureMapping::renderFrame() {
@@ -277,8 +316,8 @@ void TextureMapping::renderFrame() {
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     }
 
-    const glm::mat4 projection = _camera->getProjectionMatrix();
-    const glm::mat4 view = _camera->getViewMatrix();
+    const glm::mat4 projection = _cameras[activeCameraIndex]->getProjectionMatrix();
+    const glm::mat4 view = _cameras[activeCameraIndex]->getViewMatrix();
 
     // draw planet
     switch (_renderMode) {
@@ -288,7 +327,7 @@ void TextureMapping::renderFrame() {
         // 2. transfer mvp matrices to gpu
         _simpleShader->setUniformMat4("projection", projection);
         _simpleShader->setUniformMat4("view", view);
-        _simpleShader->setUniformMat4("model", _sphere->transform.getLocalMatrix());
+        _simpleShader->setUniformMat4("model", mario->transform.getLocalMatrix());
         // 3. enable textures and transform textures to gpu
         _simpleMaterial->mapKd->bind();
         break;
@@ -299,7 +338,7 @@ void TextureMapping::renderFrame() {
         // 2. transfer mvp matrices to gpu
         _blendShader->setUniformMat4("projection", projection);
         _blendShader->setUniformMat4("view", view);
-        _blendShader->setUniformMat4("model", _sphere->transform.getLocalMatrix());
+        _blendShader->setUniformMat4("model", mario->transform.getLocalMatrix());
         // 3. transfer light attributes to gpu
         _blendShader->setUniformVec3("light.direction", _light->transform.getFront());
         _blendShader->setUniformVec3("light.color", _light->color);
@@ -312,16 +351,13 @@ void TextureMapping::renderFrame() {
         _blendShader->setUniformFloat("material.blend", _blendMaterial->blend);
 
         // 4.3 TODO: enable textures and transform textures to gpu
-        // write your code here
-        //----------------------------------------------------------------
 
         _blendMaterial->mapKds[0]->bind(0);
         _blendShader->setUniformInt("mapKds[0]", 0);
 
         _blendMaterial->mapKds[1]->bind(1);
         _blendShader->setUniformInt("mapKds[1]", 1);
-        //----------------------------------------------------------------
-
+     
         break;
     case RenderMode::Checker:
         // 1. use the shader
@@ -329,7 +365,7 @@ void TextureMapping::renderFrame() {
         // 2. transfer mvp matrices to gpu
         _checkerShader->setUniformMat4("projection", projection);
         _checkerShader->setUniformMat4("view", view);
-        _checkerShader->setUniformMat4("model", _sphere->transform.getLocalMatrix());
+        _checkerShader->setUniformMat4("model", mario->transform.getLocalMatrix());
         // 3. transfer material attributes to gpu
         _checkerShader->setUniformInt("material.repeat", _checkerMaterial->repeat);
         _checkerShader->setUniformVec3("material.colors[0]", _checkerMaterial->colors[0]);
@@ -337,11 +373,13 @@ void TextureMapping::renderFrame() {
         break;
     }
 
-    _sphere->draw();
+    mario->draw();
 
     // draw skybox
     _skybox->draw(projection, view);
 
+    tri.draw(projection,view);
+    
     // draw ui elements
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
