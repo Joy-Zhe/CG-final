316d800936e443b0dcf49a6255088a8dfe167aa2
Jin Yuhang <128334322+jam-2002@users.noreply.github.com>
2023-05-18 10:17:01 +0800

    Update texture_mapping.h

--- src/texture_mapping.h
+++ src/texture_mapping.h
@@ -7,10 +7,10 @@
 #include "base/camera.h"
 #include "base/glsl_program.h"
 #include "base/light.h"
-#include "base/model.h"
 #include "base/skybox.h"
 #include "base/texture2d.h"
 #include "base/newmodel.h"
+#include "simplefigure.h"
 
 enum class RenderMode {
     Simple,
@@ -18,9 +18,7 @@ enum class RenderMode {
     Checker
 };
 
-struct SimpleMaterial {
-    std::shared_ptr<Texture2D> mapKd;
-};
+
 
 struct BlendMaterial {
     glm::vec3 kds[2];
@@ -40,13 +38,14 @@ public:
     ~TextureMapping();
 
 private:
-    std::unique_ptr<newmodel> _sphere;
+    //model
+    std::unique_ptr<newmodel> mario;
 
     std::unique_ptr<SimpleMaterial> _simpleMaterial;
     std::unique_ptr<BlendMaterial> _blendMaterial;
     std::unique_ptr<CheckerMaterial> _checkerMaterial;
 
-    std::unique_ptr<PerspectiveCamera> _camera;
+   // std::unique_ptr<PerspectiveCamera> _camera;
     std::unique_ptr<DirectionalLight> _light;
 
     std::unique_ptr<GLSLProgram> _simpleShader;
@@ -54,14 +53,16 @@ private:
     std::unique_ptr<GLSLProgram> _checkerShader;
 
     std::unique_ptr<SkyBox> _skybox;
+    Triangle tri;
+
+    std::vector<std::unique_ptr<Camera>> _cameras;
+    int activeCameraIndex = 0;
 
     enum RenderMode _renderMode = RenderMode::Simple;
 
     void initSimpleShader();
 
     void initBlendShader();
-
-    void initCheckerShader();
 
     void handleInput() override;
 
