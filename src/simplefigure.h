9881c5d0eeebef0b706b176fa0822f44726a3756
Jin Yuhang <128334322+jam-2002@users.noreply.github.com>
2023-05-18 10:15:43 +0800

    Update simplefigure.h

--- src/simplefigure.h
+++ src/simplefigure.h
@@ -2,20 +2,26 @@
 
 #include "base/application.h"
 #include "base/glsl_program.h"
+#include "stb_image.h"
+#include "base/texture2d.h"
 #include <memory>
 
+
+struct SimpleMaterial {
+    std::shared_ptr<Texture2D> mapKd;
+};
 struct vertex_tri {
     glm::vec3 position;
     glm::vec3 color;
 };
 
-class Triangle{
+class Triangle {
 public:
     Triangle();
 
     ~Triangle();
 
-     void draw();
+    void draw(const glm::mat4& projection, const glm::mat4& view);
 private:
     GLuint _vao = 0;
 
@@ -59,7 +65,7 @@ private:
         { glm::vec3(-0.8f, -0.9f, -0.2f), glm::vec3(0.0f, 0.0f, 0.0f)},
         { glm::vec3(-0.7f, -0.9f, -0.2f), glm::vec3(0.0f, 0.0f, 0.0f)},
         { glm::vec3(-0.7f, -0.9f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f)}
-        };
+    };
 
     unsigned int _indices[36] = {
         0, 1, 3,
@@ -82,6 +88,8 @@ private:
     };
 
     std::unique_ptr<GLSLProgram> _shader;
+    GLuint _textureID;
 
-    
+    std::unique_ptr<SimpleMaterial> simpleMaterial;
+
 };
